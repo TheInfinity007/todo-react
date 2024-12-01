2231236688a9321cbf586d9c0710b7c3c724023f
Ram Chandra <46575254+TheInfinity007@users.noreply.github.com>
2024-12-02 02:21:36 +0530

    add user input to form

--- src/components/NewTodoForm.js
+++ src/components/NewTodoForm.js
@@ -1,21 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const NewTodoForm = () => {
-  return (
-    <div className='mt-5'>
-        <form action="">
-            <div className="mb-3">
-                <label className="form-label">Assigned</label>
-                <input type="text" className='form-control' required />
-            </div>
-            <div className="mb-3">
-                <label className="form-label">Description</label>
-                <textarea className='form-control' rows={3} required />
-            </div>
-            <button className='btn btn-primary' type='button'>Add Todo</button>
-        </form>
-    </div>
-  )
-}
+    const [description, setDescription] = useState('');
+    const [assigned, setAssigned] = useState('');
 
-export default NewTodoForm
+    return (
+        <div className="mt-5">
+            <form action="">
+                <div className="mb-3">
+                    <label className="form-label">Assigned</label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        required
+                        onChange={(e) => {
+                            setAssigned(e.target.value);
+                        }}
+                        value={assigned}
+                    />
+                </div>
+                <div className="mb-3">
+                    <label className="form-label">Description</label>
+                    <textarea
+                        className="form-control"
+                        rows={3}
+                        required
+                        onChange={(e) => {
+                            setDescription(e.target.value);
+                        }}
+                        value={description}
+                    />
+                </div>
+                <button className="btn btn-primary" type="button">
+                    Add Todo
+                </button>
+            </form>
+        </div>
+    );
+};
+
+export default NewTodoForm;
