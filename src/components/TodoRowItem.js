e417a00802a73ef2023ea24bbc6d2fcf589b996f
Ram Chandra <46575254+TheInfinity007@users.noreply.github.com>
2024-12-02 01:31:26 +0530

    Passing data to components dynamically

--- src/components/TodoRowItem.js
+++ src/components/TodoRowItem.js
@@ -1,14 +1,12 @@
 import React from 'react';
 
-const TodoRowItem = () => {
-    const rowNumber = 1;
-    const rowDescriptin = 'Feed Dog';
-    const rowAssigned = 'Eric';
+const TodoRowItem = (props) => {
+    const { rowNumber, rowDescription, rowAssigned } = props;
 
     return (
         <tr>
             <th scope="row">{rowNumber}</th>
-            <td>{rowDescriptin}</td>
+            <td>{rowDescription}</td>
             <td>{rowAssigned}</td>
         </tr>
     );
