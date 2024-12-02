4b796b6c08f41ef70dfdde15a4437eebbe6cb5b8
Ram Chandra <46575254+TheInfinity007@users.noreply.github.com>
2024-12-02 22:32:17 +0530

    conditional form rendering

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import TodoTable from './components/TodoTable';
 import NewTodoForm from './components/NewTodoForm';
 
 function App() {
+    const [showAddTodoForm, setShowAddTodoForm] = useState(false);
+
     const [todos, setTodos] = useState([
         { rowNumber: 1, rowDescription: 'Feed Dog', rowAssigned: 'User One' },
         {
@@ -52,8 +54,14 @@ function App() {
                 <div className="card-header">Your Todo's</div>
                 <div className="card-body">
                     <TodoTable todos={todos} deleteTodo={deleteTodo} />
-                    <button className='btn btn-primary '>Add new todo</button>
-                    <NewTodoForm addTodo={addTodo} />
+                    <button
+                        className="btn btn-primary"
+                        onClick={() => setShowAddTodoForm(!showAddTodoForm)}
+                    >
+                        {showAddTodoForm ? 'Close New Todo': 'New Todo'}
+                    </button>
+
+                    {showAddTodoForm && <NewTodoForm addTodo={addTodo} />}
                 </div>
             </div>
         </div>
